ec1284db3d959adb098e2e578e135c8fd7f8321c
Maksim <maksim.tchava@yandex.ru>
2022-07-25 13:24:35 +0500

    fixed menu and Footer

--- src/components/commissioner/CrossIcon.vue
+++ src/components/commissioner/CrossIcon.vue
@@ -1,6 +1,14 @@
+<script setup>
+  const props = defineProps({
+    fill: {
+      type: String
+    }
+  })
+</script>
+
 <template>
   <div class="cross-icon"> 
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" height="30" width="30"><path d="m12.45 37.65-2.1-2.1L21.9 24 10.35 12.45l2.1-2.1L24 21.9l11.55-11.55 2.1 2.1L26.1 24l11.55 11.55-2.1 2.1L24 26.1Z"/></svg>
+    <svg :style="{ 'fill': props.fill }" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" height="30" width="30"><path d="m12.45 37.65-2.1-2.1L21.9 24 10.35 12.45l2.1-2.1L24 21.9l11.55-11.55 2.1 2.1L26.1 24l11.55 11.55-2.1 2.1L24 26.1Z"/></svg>
   </div>
 </template>
 
